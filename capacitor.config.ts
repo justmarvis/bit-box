69381bcdda269b064943391f1ff0f731684c8217
justmarvis <playdeadcanyon@gamil.com>
2022-07-19 14:52:05 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Bit-Box',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
