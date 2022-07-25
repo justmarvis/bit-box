cf87f5975bf9cb698eafc400fd681ce8455456eb
justmarvis <playdeadcanyon@gamil.com>
2022-07-24 19:30:58 -0600

    yes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ThemoviedbService } from '../projects/api/service/themoviedb.service';
+// import { ThemoviedbService } from '../projects/api/service/themoviedb.service';
 
 @Component({
   selector: 'app-tab1',
@@ -15,12 +15,19 @@ export class Tab1Page implements OnInit {
   animations: any;
   documentarys: any;
 
-  constructor(private service: ThemoviedbService, private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    // const movies =this.http.get('https://raw.githubusercontent.com/justmarvis/bit-box/master/src/assets/data/movies.json');
+    // movies.subscribe(
+    //   (data) => console.log(data)
+    // );
+
+    //request api data for movies
     const movies =this.http.get('https://raw.githubusercontent.com/justmarvis/bit-box/master/src/assets/data/movies.json');
     movies.subscribe(
-      (data) => console.log(data)
+      (data) => this.movies = data
     );
+
   }
 }
